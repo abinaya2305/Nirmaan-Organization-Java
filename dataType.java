95ceb2665662c792103183c890a54b45ecfe76c4
abinaya2305 <147019890+abinaya2305@users.noreply.github.com>
2025-04-03 15:55:37 +0530

    Day2

---
+++ dataType.java
@@ -0,0 +1,32 @@
+package day2;
+
+public class dataType {
+	public static void main(String[] args) {
+		
+		byte age = 56;
+		short s = 25000;
+		int i = 50000;
+		long phoneNo = 8610452025l;
+		float d = 2.25f;
+		double fu = 21.987654;
+		char ch = 56;
+		boolean bl = false;
+		
+				
+		System.out.println("byte value:" + age);
+		System.out.println("short value:" + s);
+		System.out.println("int value:" + i);
+		System.out.println("long value:" + phoneNo);
+		System.out.println("float value:" + d);
+		System.out.println("double value:" + fu);
+		System.out.println("char value:" + ch);
+		System.out.println("boolean value:" + bl);
+	}
+
+}
+
+
+
+
+
+
