72a9df846f64f9a8046ec520fe739a586805aa40
abinaya2305 <147019890+abinaya2305@users.noreply.github.com>
2025-04-10 21:09:13 +0530

    Day6

---
+++ ForLoopTask.java
@@ -0,0 +1,32 @@
+package day6;
+
+import java.util.Scanner;
+
+public class ForLoopTask {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter the number: ");
+		int iteration = sc.nextInt();
+		for(int i=1; i<=iteration; i++) {
+			if(i%4==0)
+			System.out.println(i);
+		}
+		System.out.println();
+		for(int i=1; i<=iteration; i++) {
+			if(i%4==0)
+			System.out.println("Abinaya");
+			else
+			System.out.println(i);
+		}
+		System.out.println();
+		int count = 0;
+        for (int i = 1; i <= iteration; i++) {
+            if (i % 3 == 0)
+                count++;
+        }
+        System.out.println("Total numbers divisible by 3: " + count);
+		sc.close();
+		}
+	}
+
